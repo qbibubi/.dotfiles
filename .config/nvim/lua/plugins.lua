caf257a862092f1f483848bc16bb588217d031c9
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2026-03-06 11:33:47 +0100

    update(nvim): add discord presence

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -155,5 +155,32 @@ require("lazy").setup({
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
         end,
     },
+
+    {
+        "andweeb/presence.nvim",
+        config = function()
+            require("presence").setup({
+                auto_update         = true,
+                neovim_image_text   = "The One True Text Editor",
+                main_image          = "neovim",
+                -- client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+                log_level           = nil,
+                debounce_timeout    = 10,
+                enable_line_number  = false,
+                blacklist           = {},
+                buttons             = true,
+                file_assets         = {},
+                show_time           = true,
+
+                editing_text        = "Editing %s",
+                file_explorer_text  = "Browsing %s",
+                git_commit_text     = "Committing changes",
+                plugin_manager_text = "Managing plugins",
+                reading_text        = "Reading %s",
+                workspace_text      = "Working on %s",
+                line_number_text    = "Line %s out of %s",
+            })
+        end,
+    },
 })
 
